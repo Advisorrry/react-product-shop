7defe2f63af228b9b840e3aa70660bb1e4b1c68a
advisorrry <sypikeh@yandex.com>
2021-09-28 17:02:45 +0500

    feat: add data placeholder instead of real data

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 
-export const ProductCard = () => {
+interface ProductCardProps { 
+    price: number
+    imageUrl: string
+    name: string
+}
+
+
+export const ProductCard:React.FC<ProductCardProps> = ({name, price, imageUrl}) => {
     return (
         <div className="bg-white-100 shadow-md rounded-lg border-gray-500 p-5 w-64 mb-5 transition transform hover:-translate-y-2 motion-reduce:transform-none cursor-pointer">
-            <img className="mx-auto" src="img/sneakers/1.jpg" alt="" width="133" height="112" />
+            <img className="mx-auto" src={imageUrl} alt={name} width="133" height="112" />
             <div className="">
                 <blockquote>
                     <p className="text-base	font-semibold">
-                        Мужские Кроссовки Nike Blazer Mid Suede
+                        {name}
                     </p>
                 </blockquote>
                 <figcaption className="font-medium flex justify-between ">
                     <div className="flex flex-col">
                         <div className="text-cyan-600">Цена:</div>
-                        <div className="text-base font-semibold">12 999 руб.</div>
+                        <div className="text-base font-semibold">{price} руб.</div>
                     </div>
                     <button>
-                        <img src="img/add-btn.svg" alt="" />
+                        <img src='img/add-btn.svg' alt='Добавить в корзину' />
                     </button>
                 </figcaption>
             </div>
